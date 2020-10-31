5ca87c2a928b9dfde9365f2dd686e5f3847c1d08
bmoya13 <71987706+bmoya13@users.noreply.github.com>
2020-10-30 17:15:29 -0700

    Follow Feature

---
+++ Assets/BrandonM_WIP/Scripts/Friend2Follow.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+using System.Threading;
+using UnityEngine;
+
+public class Friend2Follow : MonoBehaviour
+{
+    public float speed;
+
+    private Transform target;
+
+    public GameObject mainplayer;
+    public GameObject friend1;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Friend1").GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Physics2D.IgnoreCollision(mainplayer.GetComponent<Collider2D>(), friend1.GetComponent<Collider2D>());
+        if (Vector2.Distance(transform.position, target.position) > 2)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        }
+    }
+}
