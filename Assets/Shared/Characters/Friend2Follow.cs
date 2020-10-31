60b05a44f810e720410063533ab9098d5afbea75
joshua-maros <60271685+joshua-maros@users.noreply.github.com>
2020-10-30 17:58:32 -0700

    Integrated changes.

--- Assets/Shared/Characters/Friend2Follow.cs
+++ Assets/Shared/Characters/Friend2Follow.cs
@@ -5,26 +5,28 @@ using System.Security.Cryptography;
 using System.Threading;
 using UnityEngine;
 
-public class Friend1Follow : MonoBehaviour
+public class Friend2Follow : MonoBehaviour
 {
     public float speed;
 
     private Transform target;
 
-    public GameObject mainplayer;
-    public GameObject friend2;
+    private GameObject mainplayer;
+    private GameObject friend1;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        target = GameObject.FindGameObjectWithTag("Friend1").GetComponent<Transform>();
+        mainplayer = GlobalData.instance.mainPlayer.gameObject;
+        friend1 = GlobalData.instance.friend1.gameObject;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Physics2D.IgnoreCollision(mainplayer.GetComponent<Collider2D>(), friend2.GetComponent<Collider2D>());
+        Physics2D.IgnoreCollision(mainplayer.GetComponent<Collider2D>(), friend1.GetComponent<Collider2D>());
         if (Vector2.Distance(transform.position, target.position) > 2)
         {
             transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
---
+++ Assets/Shared/Scripts/FollowCamera.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    public GameObject target = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 newPos = Vector3.Lerp(transform.position, target.transform.position, Time.deltaTime * 2.0f);
+        if ((target.transform.position - transform.position).magnitude < 0.05f) {
+            newPos = target.transform.position;
+        }
+        newPos.z = transform.position.z;
+        transform.position = newPos;
+    }
+}
